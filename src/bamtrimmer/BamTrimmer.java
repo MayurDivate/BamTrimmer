00c786101d1ff2fa7374b5721ee452087a1d0eb8
Mayur Divate <mayur.divate91@gmail.com>
2019-10-02 10:32:18 +1000

    first push

---
+++ src/bamtrimmer/BamTrimmer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bamtrimmer;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author n10337547
+ */
+public class BamTrimmer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mainwindow = new MainFrame();
+        mainwindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainwindow.setVisible(true);
+    }
+    
+}
